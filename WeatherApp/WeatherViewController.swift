92a7c36529fe0536b9a31643477bbab8e6045e2a
Alexandra Matveeva <alexa.matveeva.dev@gmail.com>
2022-08-26 17:11:54 +0300

    feat: add locationButton, searchButton and searchTextField to the top

--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class WeatherViewController: UIViewController {
     
     let backgroungView = UIImageView()
+    let locationButton = UIButton()
+    let searchButton = UIButton()
+    let searchTextField = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +28,56 @@ extension WeatherViewController {
         backgroungView.image = UIImage(named: "background")
         backgroungView.contentMode = .scaleAspectFill
         
+        locationButton.translatesAutoresizingMaskIntoConstraints = false
+        locationButton.setBackgroundImage(UIImage(systemName: "location.circle.fill"), for: .normal)
+        locationButton.tintColor = .label
+        
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.setBackgroundImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchButton.tintColor = .label
+        
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchTextField.font = UIFont.preferredFont(forTextStyle: .title1)
+        searchTextField.placeholder = "Search"
+        searchTextField.textAlignment = .right
+        searchTextField.borderStyle = .roundedRect
+        searchTextField.backgroundColor = .systemFill
+        
     }
     
     func layout() {
         view.addSubview(backgroungView)
+        view.addSubview(locationButton)
+        view.addSubview(searchButton)
+        view.addSubview(searchTextField)
         
         NSLayoutConstraint.activate([
             backgroungView.topAnchor.constraint(equalTo: view.topAnchor),
             backgroungView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroungView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroungView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            locationButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            locationButton.widthAnchor.constraint(equalToConstant: 40),
+            locationButton.heightAnchor.constraint(equalToConstant: 40)
+            
+        ])
+        
+        NSLayoutConstraint.activate([
+            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: searchButton.trailingAnchor, multiplier: 1),
+            searchButton.widthAnchor.constraint(equalToConstant: 40),
+            searchButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: locationButton.trailingAnchor, multiplier: 1),
+            searchButton.leadingAnchor.constraint(equalToSystemSpacingAfter: searchTextField.trailingAnchor, multiplier: 1),
+            
         ])
         
     }
